211357a9f2a7305ec412fc50e8b2c30595a34ec2
Aigimik <118675541+Aigimik@users.noreply.github.com>
2023-03-17 22:12:01 +0300

    Add files via upload

---
+++ 15/js/popup.js
@@ -0,0 +1,22 @@
+let loginButton = document.querySelector(".login-button");
+let loginPopUp = document.querySelector(".login-popup");
+let closseButton = document.querySelector(".popup-clos");
+
+loginButton.addEventListener("click", showPopup);
+closeButton.addEventListener("click", close);
+
+function showPopup(evt) {
+  console.log(evt);
+  evt.preventDefault();
+
+  loginPopUp.classList.toggle("show-popup");
+}
+
+function close() {
+  loginPopUp.classList.remove("show-popup");
+}
+
+loginPopUp.addEventListener("click", (e) => {
+  if (e.target.classList.contains("show-popup")) {
+  }
+});
